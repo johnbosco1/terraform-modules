4937171cea286f4e5aef81c5bd81e0af3b2945e8
johnbosco1 <37475536+johnbosco1@users.noreply.github.com>
2025-05-21 12:56:35 +0000

    Update main.tf

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -1,1 +1,33 @@
 
+resource "azurerm_resource_group" "rg" {
+  name = var.name
+  location = var.location
+  tags = var.tags
+}
+
+output "id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "location" {
+  value = azurerm_resource_group.rg.location
+}
+
+variable "name" {
+  description = "The name of the resource group in which the resources will be created."
+  type = string
+}
+
+variable "location" {
+  description = "The location of the resource group in which the resources will be created."
+  type = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource group."
+  type = map(string)
+}
